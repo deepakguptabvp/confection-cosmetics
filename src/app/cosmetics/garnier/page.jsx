42bd574447b19098d045af98a3420068bed64a8a
deepakguptabvp <deepakgupta.150743@gmail.com>
2024-01-18 21:34:15 +0530

    Working on Garnier page

--- src/app/cosmetics/garnier/page.jsx
+++ src/app/cosmetics/garnier/page.jsx
@@ -1,9 +1,171 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const page = () => {
   return (
-    <div>garnier color</div>
-  )
-}
+    <div>
+      <section className="text-stone-600  body-font">
+        <div className="container p-5">
+          <div className="flex flex-wrap -m-4">
 
-export default page
+            {/* All Color Items */}
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center m-auto md:m-0 w-full h-full block"
+                  src="/Garnier-1.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-5 font-medium">
+                Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
+                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
+              </h2>
+              <p className="mt-1">MRP: ₹220</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/Garnier-3.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
+                Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
+                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
+              </h2>
+              <p className="mt-1">MRP: ₹220</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/Garnier-4.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
+                Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
+                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
+              </h2>
+              <p className="mt-1">MRP: ₹220</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/Garnier-5.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
+                Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
+                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
+              </h2>
+              <p className="mt-1">MRP: ₹220</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/Garnier-1.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
+                Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
+                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
+              </h2>
+              <p className="mt-1">MRP: ₹220</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/Garnier-3.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
+                Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
+                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
+              </h2>
+              <p className="mt-1">MRP: ₹220</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/Garnier-4.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
+                Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
+                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
+              </h2>
+              <p className="mt-1">MRP: ₹220</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full  block"
+                  src="/Garnier-5.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+                Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
+                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
+              </h2>
+              <p className="mt-1">MRP: ₹220</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default page;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { FaShoppingCart } from "react-icons/fa";
 const Navbar = () => {
   return (
     <div>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 shadow-md">
+      <nav className="bg-white border-gray-200 dark:bg-stone-500 shadow-md">
         <div className="flex flex-col md:flex-row flex-wrap gap-4 justify-between items-center mx-auto max-w-screen-xl p-2">
           <Link
             href="/"
@@ -26,10 +26,51 @@ const Navbar = () => {
             </span>
           </Link>
 
-          <div>
-            <div className="max-w-screen-xl px-4 py-3 mx-auto">
-              <div className="flex items-center">
-                <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+          {/* <div> */}
+            <div className="max-w-screen-xl  px-2 py-1">
+              <div className="flex flex-auto items-center">
+                <form>
+                  <label
+                    htmlFor="default-search"
+                    className=" text-sm font-medium sr-only bg-stone-500 dark:text-white"
+                  >
+                    Search
+                  </label>
+                  <div className="relative">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                      <svg
+                        className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                        />
+                      </svg>
+                    </div>
+                    <input
+                      type="search"
+                      id="default-search"
+                      className="block w-96 p-4 ps-10 text-sm  border border-stone-300 rounded-lg bg-stone-50 focus:ring-stone-500 focus:border-stone-500 dark:bg-gray-300 dark:border-stone-600 dark:placeholder-stone-400 dark:text-black dark:focus:ring-stone-500 dark:focus:border-stone-500"
+                      placeholder="Search Cosmetics, Confectionery..."
+                      required
+                    />
+                    <button
+                      type="submit"
+                      className="text-white absolute end-2 top-2 bottom-2 bg-stone-700 hover:bg-stone-500 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-stone-400 dark:hover:bg-stone-700 dark:focus:ring-stone-700"
+                    >
+                      Search
+                    </button>
+                  </div>
+                </form>
+
+                {/* <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
                   <li>
                     <Link
                       href="/confectionery"
@@ -64,24 +105,40 @@ const Navbar = () => {
                       Contact
                     </Link>
                   </li>
-                </ul>
+                </ul> */}
               </div>
             </div>
-          </div>
+          {/* </div> */}
 
-          <div className="flex items-center space-x-6 rtl:space-x-reverse">
-            <button>
-              <FaShoppingCart className="text-xl md:text-2xl" />
+          <div className="flex items-center space-x-6 rtl:space-x-reverse mr-3">
+            {/* <button>
+              <FaShoppingCart className="text-xl md:text-2xl dark:text-white" />
+            </button> */}
+            <button
+              type="button"
+              className="text-white bg-stone-700  hover:bg-stone-800 focus:ring-4 focus:outline-none focus:ring-stone-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-stone-400 dark:hover:bg-stone-700 dark:focus:ring-stone-400"
+            >
+              <svg
+                className="w-3.5 h-3.5 me-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 18 21"
+              >
+                <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
+              </svg>
+              Buy now
             </button>
-            <Link
+
+            {/* <Link
               href="tel:5541251234"
               className="text-sm  text-gray-500 dark:text-white hover:underline"
             >
               (555) 412-1234
-            </Link>
+            </Link> */}
             <Link
-              href="#"
-              className="text-lg text-blue-600 dark:text-blue-500 hover:underline"
+              href="/"
+              className="text-lg text-stone-900  dark:invert  hover:underline"
             >
               Login
             </Link>
@@ -90,8 +147,8 @@ const Navbar = () => {
       </nav>
 
       {/* Navbar menu Home/About/contact.... */}
-      <div className="bg-gray-50 dark:bg-gray-700">
-        <div className="max-w-screen-xl p-3 flex md:flex-row flex-wrap justify-center bg-slate-300 mx-auto">
+      <div className="bg-stone-300 shadow-md dark:bg-stone-500">
+        <div className="max-w-screen-xl p-3 flex md:flex-row flex-wrap justify-center  dark:bg-stone-700 mx-auto">
           <div className="flex justify-center">
             <ul className="flex flex-wrap gap-10 font-medium mt-0 text-sm">
               <li>
@@ -137,7 +194,7 @@ const Navbar = () => {
               </li>
               <li>
                 <Link
-                  href="/confectionery/dove"
+                  href="/cosmetics/dove"
                   className="text-gray-900 dark:text-white hover:underline"
                 >
                   Dove
@@ -145,7 +202,7 @@ const Navbar = () => {
               </li>
               <li>
                 <Link
-                  href="/confectionery/loreal"
+                  href="/cosmetics/loreal"
                   className="text-gray-900 dark:text-white hover:underline"
                 >
                   Loreal
@@ -153,7 +210,7 @@ const Navbar = () => {
               </li>
               <li>
                 <Link
-                  href="/confectionery/lotus"
+                  href="/cosmetics/lotus"
                   className="text-gray-900 dark:text-white hover:underline"
                 >
                   Lotus
@@ -161,7 +218,7 @@ const Navbar = () => {
               </li>
               <li>
                 <Link
-                  href="/confectionery/garnier"
+                  href="/cosmetics/garnier"
                   className="text-gray-900 dark:text-white hover:underline"
                 >
                   Garnier
@@ -169,7 +226,7 @@ const Navbar = () => {
               </li>
               <li>
                 <Link
-                  href="/confectionery/indulekha"
+                  href="/cosmetics/indulekha"
                   className="text-gray-900 dark:text-white hover:underline"
                 >
                   Indulekha
