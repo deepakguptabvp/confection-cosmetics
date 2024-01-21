6b922ddda6d6764d6b395f945b6c067cc2d948a7
deepakguptabvp <deepakgupta.150743@gmail.com>
2024-01-21 21:23:10 +0530

    Working on Loreal page

--- src/app/cosmetics/garnier/page.jsx
+++ src/app/cosmetics/garnier/page.jsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
-    <div>
+    <div className="min-h-lvh">
       <section className="text-stone-600  body-font">
         <div className="container p-5">
-          <div className="flex flex-wrap -m-4">
+          <div className="flex flex-wrap justify-evenly mx-auto my-auto">
 
             {/* All Color Items */}
-
             <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              {/* <Link href={'/cosmetics/garnier/garnier-1'}> */}
               <a className="block relative rounded overflow-hidden">
                 <Image
                   alt="ecommerce"
@@ -21,12 +22,12 @@ const page = () => {
                 />
               </a>
 
-              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
               <h2 className="text-stone-900 title-font text-sm mt-5 font-medium">
                 Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
                 Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
               </h2>
-              <p className="mt-1">MRP: ₹220</p>
+              <p className="mt-1 font-bold">MRP: ₹220</p>
+            {/* </Link> */}
             </div>
 
             <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
@@ -40,12 +41,11 @@ const page = () => {
                 />
               </a>
 
-              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
               <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
                 Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
-                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
+                Shine, Color Naturals, Shade: 3 Darkest Brown, 70ml + 60g
               </h2>
-              <p className="mt-1">MRP: ₹220</p>
+              <p className="mt-1 font-bold">MRP: ₹220</p>
             </div>
 
             <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
@@ -59,12 +59,11 @@ const page = () => {
                 />
               </a>
 
-              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
               <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
                 Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
-                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
+                Shine, Color Naturals, Shade: 4 Brown, 70ml + 60g
               </h2>
-              <p className="mt-1">MRP: ₹220</p>
+              <p className="mt-1 font-bold">MRP: ₹220</p>
             </div>
 
             <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
@@ -78,12 +77,31 @@ const page = () => {
                 />
               </a>
 
-              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
               <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
                 Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
-                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
+                Shine, Color Naturals, Shade: 5 Light Brown, 70ml + 60g
               </h2>
-              <p className="mt-1">MRP: ₹220</p>
+              <p className="mt-1 font-bold">MRP: ₹220</p>
+            </div>
+
+
+            {/* Added Extra to check the padding and style... */}
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/Garnier-3.16.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
+                Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
+                Shine, Color Naturals, Shade: 3.16 Burgundy, 70ml + 60g
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹220</p>
             </div>
 
             <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
@@ -91,40 +109,20 @@ const page = () => {
                 <Image
                   alt="ecommerce"
                   className="object-cover object-center w-full h-full block"
-                  src="/Garnier-1.jpg"
+                  src="/Garnier-4.20.jpg"
                   width={200}
                   height={200}
                 />
               </a>
 
-              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
               <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
                 Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
-                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
+                Shine, Color Naturals, Shade: 4.20 Vine Burgundy, 70ml + 60g
               </h2>
-              <p className="mt-1">MRP: ₹220</p>
+              <p className="mt-1 font-bold">MRP: ₹220</p>
             </div>
 
-            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
-              <a className="block relative rounded overflow-hidden">
-                <Image
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="/Garnier-3.jpg"
-                  width={200}
-                  height={200}
-                />
-              </a>
-
-              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
-              <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
-                Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
-                Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
-              </h2>
-              <p className="mt-1">MRP: ₹220</p>
-            </div>
-
-            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+            {/* <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
               <a className="block relative rounded overflow-hidden">
                 <Image
                   alt="ecommerce"
@@ -135,12 +133,11 @@ const page = () => {
                 />
               </a>
 
-              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
               <h2 className="text-stone-900 title-font text-sm mt-1 font-medium">
                 Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
                 Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
               </h2>
-              <p className="mt-1">MRP: ₹220</p>
+              <p className="mt-1 font-bold">MRP: ₹220</p>
             </div>
 
             <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
@@ -154,13 +151,14 @@ const page = () => {
                 />
               </a>
 
-              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
               <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
                 Garnier, Hair Colouring Creme, Long-lasting Colour, Smoothness &
                 Shine, Color Naturals, Shade: 1 Natural Black, 70ml + 60g
               </h2>
-              <p className="mt-1">MRP: ₹220</p>
-            </div>
+              <p className="mt-1 font-bold">MRP: ₹220</p>
+            </div> */}
+
+            
           </div>
         </div>
       </section>
--- src/app/cosmetics/loreal/page.jsx
+++ src/app/cosmetics/loreal/page.jsx
@@ -1,8 +1,257 @@
 import React from 'react'
+import Image from 'next/image'
 
 const page = () => {
   return (
-    <div className='min-h-lvh'>page</div>
+    <div className='min-h-lvh'>
+      <section className="text-stone-600  body-font">
+        <div className="container p-5">
+          <div className="flex flex-wrap justify-evenly mx-auto my-auto">
+
+            {/* All Color Items */}
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center m-auto md:m-0 w-full block"
+                  src="/casting-200.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+                L'Oréal Paris Semi-Permanent Hair Colour, Ammonia-Free Formula & Honey-Infused Conditioner, Glossy Finish, Casting Crème Gloss, Ebony Black 200, 87.5g+72ml
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹649.00</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/casting-300.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+                L'Oréal Paris Semi-Permanent Hair Colour, Ammonia-Free Formula & Honey-Infused Conditioner, Glossy Finish, Casting Crème Gloss, Darkest Brown 300, 87.5g+72ml
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹649.00</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/casting-400.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+                L'Oreal Paris Semi-Permanent Hair Colour, Ammonia-Free Formula & Honey-Infused Conditioner, Glossy Finish, Casting Creme Gloss, Dark Brown 400, 87.5g+72ml
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹679.00</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/casting-500.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+                L'Oreal Paris Semi-Permanent Hair Colour, Ammonia-Free Formula & Honey-Infused Conditioner, Glossy Finish, Casting Crème Gloss, Medium Brown 500, 87.5g+72ml
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹679.00</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/casting-316.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+                L'Oréal Paris Semi-Permanent Hair Colour, Ammonia-Free Formula & Honey-Infused Conditioner, Glossy Finish, Casting Crème Gloss, Plum/Burgundy 316, 87.5g+72ml
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹649.00</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/casting-415.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+                L'Oreal Paris Semi-Permanent Hair Colour, Ammonia-Free Formula & Honey-Infused Conditioner, Glossy Finish, Casting Crème Gloss, Iced Chocolate 415, 87.5g+72ml
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹649.00</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/casting-535.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+                L'Oréal Paris Semi-Permanent Hair Colour, Ammonia-Free Formula & Honey-Infused Conditioner, Glossy Finish, Casting Crème Gloss, 535 Chocolate, 87.5g+72ml
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹679.00</p>
+            </div>
+
+            {/* Excellence products - 1,3,4,5,3.16,4.25 */}
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/excellence-1.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+                L'Oréal Paris Permanent Hair Colour, Radiant At-Home Hair Colour with up to 100% Grey Coverage, Pro-Keratin, Up to 8 Weeks of Colour, Excellence Crème, 1 Black, 72ml+100g
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹679.00</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/excellence-3.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+                L'Oreal Paris Permanent Hair Colour, Radiant At-Home Hair Colour with up to 100% Grey Coverage, Pro-Keratin, Up to 8 Weeks of Colour, Excellence Crème, 3 Dark Brown, 72ml+100g
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹679.00</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/excellence-4.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+                L'Oréal Paris Permanent Hair Colour, Radiant At-Home Hair Colour with up to 100% Grey Coverage, Pro-Keratin, Up to 8 Weeks of Colour, Excellence Crème, 4 Natural Dark Brown, 72ml+100g
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹679.00</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/excellence-5.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+              L'Oréal Paris Permanent Hair Colour, Radiant At-Home Hair Colour with up to 100% Grey Coverage, Pro-Keratin, Up to 8 Weeks of Colour, Excellence Crème, 5 Light Brown, 72ml+100g
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹679.00</p>
+            </div>
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/excellence-3.16.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+                L'Oréal Paris Permanent Hair Colour, Radiant At-Home Hair Colour with up to 100% Grey Coverage, Pro-Keratin, Up to 8 Weeks of Colour, Excellence Crème, 3.16 Burgundy, 72ml+100g
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹679.00</p>
+            </div>
+
+
+            <div className="lg:w-1/5 md:w-1/2 p-2 text-left rounded-lg border border-transparent m-5 px-5 py-5 transition-colors hover:border-stone-400 hover:bg-stone-200 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full shadow-2xl bg-white">
+              <a className="block relative rounded overflow-hidden">
+                <Image
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/excellence-4.25.jpg"
+                  width={200}
+                  height={200}
+                />
+              </a>
+
+              {/* <h3 className="text-stone-500 mt-3 text-xs tracking-widest title-font mb-1">CATEGORY</h3> */}
+              <h2 className="text-stone-900 title-font text-sm mt-2 font-medium">
+              L'Oreal Paris Permanent Hair Colour, Radiant At-Home Hair Colour with up to 100% Grey Coverage, Pro-Keratin, Up to 8 Weeks of Colour, Excellence Crème, 4.25 Aishwarya'S Brown, 72ml+100g
+              </h2>
+              <p className="mt-1 font-bold">MRP: ₹679.00</p>
+            </div>
+
+
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
